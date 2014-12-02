42140ee22147b6e5851ff459effb65dd65ea7973
tianbiao <2502326938@qq.com>
2014-12-02 22:03:00 +0800

    function for delete complete

---
+++ DiskList.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+	BindToView(disks);
+});
+
+function BindToView(disks){
+	$("#container").empty();
+	var diskDivs = _.map(disks,function(disk){
+	return diskDiv(disk);
+	});
+	_.each(diskDivs,function(div){
+	$("#container").append(div);
+	});
+}
+
+function diskDiv(disk){
+return $("<div>")
+.attr("class","disk1")
+.append(diskTitle(disk.name))
+.append(diskImg(disk.img))
+.append(diskDesc(disk.desc))
+//.append(diskRemove())
+.append(diskButton1(disk.number))
+;
+}
+
+function diskTitle(title){
+return $("<h3>").html(title);
+}
+
+function diskImg(img){
+return $("<img>").attr("src",img);
+}
+
+function diskDesc(desc){
+return $("<p>").html(desc);
+}
+
+function diskButton1(number){
+return $("<button>").attr("id",number).attr("onclick","rem(this.id)").html("删除");
+}
+
+function rem(id){
+	var i=0;
+	while(disks[i].number!=id)
+	{
+		i++;
+	}
+	disks.splice(i,1);
+	BindToView(disks);
+}
+
+ 
+ 
+function add(){
+ var newProduct ={
+ name: $("#productName").val(),
+ img:  $("#productImg").val(),
+ desc: $("#productDesc").val(),
+ number: disks.length
+ };
+
+ disks.push(newProduct);
+ BindToView(disks);
+ 
+ }
+ 
+ function search(){
+ var keyword=$("#search").val();
+ var searchResult = _.filter(disks,function(disk){
+  return disk.name.indexOf(keyword)!=-1;
+  });
+  console.log(searchResult[0]);
+  BindToView(searchResult);
+ 
+ }
+ 
+ 
+ 
+ 
---
+++ FakeData.js
@@ -0,0 +1,5 @@
+var disks=[
+{name: "小清新光盘", img:"./pics/disk/fancy-disk.jpg",desc:"小清新、小文艺 35元/10张", number:0 },
+{name: "婚庆光盘",img: "./pics/disk/marriage-disk.jpg", desc: "记录你的美好瞬间 50元/10张", number: 1  },
+{name: "1TB大容量光盘", img:"./pics/disk/1TB-disk.jpg", desc: "解放你的硬盘  100元/0张", number: 2 }
+];
